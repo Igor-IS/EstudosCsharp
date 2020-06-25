8ee72d9840ae808773ce43ffc0d5ef8d03a32383
Igor-IS <igorvulkan@gmail.com>
2020-06-25 14:36:44 -0300

    Projeto refatorado

---
+++ LojaVirtual/CustomerShoppings/CustomerShoppingList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using VirtualStore.Products;
+using System.Text;
+
+namespace VirtualStore.CustomerShoppings
+{
+    class CustomerShoppingList
+    {
+        internal double AmoutToPay { get; set; }
+        internal List<Product> ShoppingList { get; set; }
+        internal Product Produto { get; set; }
+        internal int QuantityTheProduct { get; set; }
+        internal CustomerShoppingList() { }
+
+        internal CustomerShoppingList(Product product)
+        {
+            Produto = product;
+        }
+
+        internal void GetProduct()
+        {
+
+        }
+
+
+        internal void RemoveProduct(string nomeProduto)
+        {
+            
+        }
+
+
+    }
+}
---
+++ LojaVirtual/PaymentMethods/CashPayment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VirtualStore.PaymentMethods
+{
+    class CashPayment 
+    {
+        internal Payment Payment { get; set; }
+        internal const double Descount = 0.10;
+
+        internal CashPayment() { }
+        internal CashPayment(Payment payment)
+        {
+            Payment = payment;
+            GetDiscount();
+        }
+
+        private void GetDiscount()
+        {
+            GetFullValueDiscount();
+            DisplayValue();
+        }
+
+        private double GetDiscountAmount(double valorDesconto) =>  Payment.ShoppingListValue * valorDesconto;
+
+        private void GetFullValueDiscount() => Payment.TotalPayable -= GetDiscountAmount(Descount);
+
+        private void DisplayValue() => Console.WriteLine($"\nValor Total a vista R${Payment.TotalPayable:F2}");
+    }
+}
